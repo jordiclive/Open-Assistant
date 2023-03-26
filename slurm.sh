444d94da81a30589d35cb4a6632790f2a56a103c
jordiclive <jordiclive19@imperial.ac.uk>
2023-03-26 13:42:15 +0100

    no message

--- slurm.sh
+++ slurm.sh
@@ -13,6 +13,7 @@ cd /admin/home-jordiclive/Open-Assistant
 export TRANSFORMERS_CACHE=/admin/home-jordiclive/transformers_cache
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 export PYTHONPATH="/admin/home-jordiclive/Open-Assistant/model/model_training:$PYTHONPATH"
+export MASTER_PORT="44093"
 cd /admin/home-jordiclive/Open-Assistant/model/model_training/
 srun deepspeed /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu gpt-neox --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --num_train_epochs 8 --residual_dropout 0.2 --deepspeed --num_train_epochs 12 --gradient_accumulation_steps 1 --use_flash_attention false --residual_dropout 0.0 --learning_rate 4e-6
 
