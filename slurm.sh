aafd1300859ecfca37ef17e3761106f5d0bd4b3b
jordiclive <jordiclive19@imperial.ac.uk>
2023-03-26 12:20:07 +0100

    add slurm script

---
+++ slurm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account laion
+#SBATCH --partition="g80"
+#SBATCH --job-name=OA
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --output=%x_%j.out
+
+
+
+source /admin/home-jordiclive/miniconda3/bin/activate open_assistant
+cd /admin/home-jordiclive/Open-Assistant/model/model_training
+export TRANSFORMERS_CACHE=/admin/home-jordiclive/transformers_cache
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+srun deepspeed trainer_sft.py --configs defaults oasst_export_eu gpt-neox --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --num_train_epochs 8 --residual_dropout 0.2 --deepspeed --num_train_epochs 12 --gradient_accumulation_steps 1 --use_flash_attention false --residual_dropout 0.0 --learning_rate 4e-6
+
+
+
