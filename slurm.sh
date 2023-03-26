485ec9bfacc316184538a0f21aed017fbbeda3f4
jordiclive <jordiclive19@imperial.ac.uk>
2023-03-26 14:53:42 +0100

    no message

--- slurm.sh
+++ slurm.sh
@@ -42,10 +42,7 @@ export MASTER_PORT="12802"
 master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_ADDR=$master_addr"i"
 echo "MASTER_ADDR="$MASTER_ADDR
-
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
-
-
 
 cd /admin/home-jordiclive/Open-Assistant/model/model_training/
 srun deepspeed /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu gpt-neox --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --num_train_epochs 8 --residual_dropout 0.2 --deepspeed --num_train_epochs 12 --gradient_accumulation_steps 1 --use_flash_attention false --residual_dropout 0.0 --learning_rate 4e-6
