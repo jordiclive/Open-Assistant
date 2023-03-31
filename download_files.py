299e04b568826964473b169c6a88a13710c203e8
jordiclive <jordiclive19@imperial.ac.uk>
2023-03-31 09:49:32 +0100

    no message

---
+++ download_files.py
@@ -0,0 +1,12 @@
+files =
+
+['2023-02-19_oasst_ready_with_spam_deleted.jsonl',
+'2023-02-19_oasst_ready_with_spam_deleted.jsonl.gz'
+2023-02-26_oasst_default.jsonl.gz
+2023-03-07_oasst_default_with_labels.jsonl.gz
+
+/Users/jordanclive/Desktop/oa_dataset_2
+
+
+for k in files:
+
