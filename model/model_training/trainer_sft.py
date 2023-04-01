aecefc870785393cb3f2ef29719b8e2dab8de7c5
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-01 23:03:22 +0100

    try flash attn

--- model/model_training/trainer_sft.py
+++ model/model_training/trainer_sft.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import os
+from model_training.models.llama_monkey_patch import replace_llama_attn_with_flash_attn
+replace_llama_attn_with_flash_attn()
 from typing import List
 
 import time
@@ -124,72 +126,72 @@ def preprocess_logits_for_metrics(logits, labels):
     pred_ids = torch.argmax(logits, dim=-1)
     return pred_ids
 
-from transformers import AdamW
-import math
-from transformers.trainer_pt_utils import get_parameter_names
-from transformers.pytorch_utils import ALL_LAYERNORM_LAYERS
-class CustomAdamW(AdamW):
-    def step(self, closure: Callable = None):
-        """
-        Performs a single optimization step.
-        Arguments:
-            closure (`Callable`, *optional*): A closure that reevaluates the model and returns the loss.
-        """
-        loss = None
-        if closure is not None:
-            loss = closure()
-
-        for group in self.param_groups:
-            for p in group["params"]:
-                if p.grad is None:
-                    continue
-                if 'lora' not in p.grad_fn.variable.name:
-                    continue
-                grad = p.grad.data
-                if grad.is_sparse:
-                    raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
-
-                state = self.state[p]
-
-                # State initialization
-                if len(state) == 0:
-                    state["step"] = 0
-                    # Exponential moving average of gradient values
-                    state["exp_avg"] = torch.zeros_like(p.data)
-                    # Exponential moving average of squared gradient values
-                    state["exp_avg_sq"] = torch.zeros_like(p.data)
-
-                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
-                beta1, beta2 = group["betas"]
-
-                state["step"] += 1
-
-                # Decay the first and second moment running average coefficient
-                # In-place operations to update the averages at the same time
-                exp_avg.mul_(beta1).add_(grad, alpha=(1.0 - beta1))
-                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1.0 - beta2)
-                denom = exp_avg_sq.sqrt().add_(group["eps"])
-
-                step_size = group["lr"]
-                if group["correct_bias"]:  # No bias correction for Bert
-                    bias_correction1 = 1.0 - beta1 ** state["step"]
-                    bias_correction2 = 1.0 - beta2 ** state["step"]
-                    step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
-
-                p.data.addcdiv_(exp_avg, denom, value=-step_size)
-
-                # Just adding the square of the weights to the loss function is *not*
-                # the correct way of using L2 regularization/weight decay with Adam,
-                # since that will interact with the m and v parameters in strange ways.
-                #
-                # Instead we want to decay the weights in a manner that doesn't interact
-                # with the m/v parameters. This is equivalent to adding the square
-                # of the weights to the loss with plain (non-momentum) SGD.
-                # Add weight decay at the end (fixed version)
-                if group["weight_decay"] > 0.0:
-                    p.data.add_(p.data, alpha=(-group["lr"] * group["weight_decay"]))
-
-        return loss
+# from transformers import AdamW
+# import math
+# from transformers.trainer_pt_utils import get_parameter_names
+# from transformers.pytorch_utils import ALL_LAYERNORM_LAYERS
+# class CustomAdamW(AdamW):
+#     def step(self, closure: Callable = None):
+#         """
+#         Performs a single optimization step.
+#         Arguments:
+#             closure (`Callable`, *optional*): A closure that reevaluates the model and returns the loss.
+#         """
+#         loss = None
+#         if closure is not None:
+#             loss = closure()
+#
+#         for group in self.param_groups:
+#             for p in group["params"]:
+#                 if p.grad is None:
+#                     continue
+#                 if 'lora' not in p.grad_fn.variable.name:
+#                     continue
+#                 grad = p.grad.data
+#                 if grad.is_sparse:
+#                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
+#
+#                 state = self.state[p]
+#
+#                 # State initialization
+#                 if len(state) == 0:
+#                     state["step"] = 0
+#                     # Exponential moving average of gradient values
+#                     state["exp_avg"] = torch.zeros_like(p.data)
+#                     # Exponential moving average of squared gradient values
+#                     state["exp_avg_sq"] = torch.zeros_like(p.data)
+#
+#                 exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
+#                 beta1, beta2 = group["betas"]
+#
+#                 state["step"] += 1
+#
+#                 # Decay the first and second moment running average coefficient
+#                 # In-place operations to update the averages at the same time
+#                 exp_avg.mul_(beta1).add_(grad, alpha=(1.0 - beta1))
+#                 exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1.0 - beta2)
+#                 denom = exp_avg_sq.sqrt().add_(group["eps"])
+#
+#                 step_size = group["lr"]
+#                 if group["correct_bias"]:  # No bias correction for Bert
+#                     bias_correction1 = 1.0 - beta1 ** state["step"]
+#                     bias_correction2 = 1.0 - beta2 ** state["step"]
+#                     step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
+#
+#                 p.data.addcdiv_(exp_avg, denom, value=-step_size)
+#
+#                 # Just adding the square of the weights to the loss function is *not*
+#                 # the correct way of using L2 regularization/weight decay with Adam,
+#                 # since that will interact with the m and v parameters in strange ways.
+#                 #
+#                 # Instead we want to decay the weights in a manner that doesn't interact
+#                 # with the m/v parameters. This is equivalent to adding the square
+#                 # of the weights to the loss with plain (non-momentum) SGD.
+#                 # Add weight decay at the end (fixed version)
+#                 if group["weight_decay"] > 0.0:
+#                     p.data.add_(p.data, alpha=(-group["lr"] * group["weight_decay"]))
+#
+#         return loss
 
 
 class SFTTrainer(Trainer):
@@ -309,31 +311,31 @@ class SFTTrainer(Trainer):
         )
         return dataloader
 
-    def create_optimizer(self):
-        opt_model = self.model
-        if self.optimizer is None:
-            decay_parameters = get_parameter_names(opt_model, ALL_LAYERNORM_LAYERS)
-            decay_parameters = [name for name in decay_parameters if "bias" not in name]
-            optimizer_grouped_parameters = [
-                {
-                    "params": [
-                        p for n, p in opt_model.named_parameters() if (n in decay_parameters and p.requires_grad)
-                    ],
-                    "weight_decay": self.args.weight_decay,
-                },
-                {
-                    "params": [
-                        p for n, p in opt_model.named_parameters() if (n not in decay_parameters and p.requires_grad)
-                    ],
-                    "weight_decay": 0.0,
-                },
-            ]
-
-            optimizer_cls, optimizer_kwargs = Trainer.get_optimizer_cls_and_kwargs(self.args)
-
-            self.optimizer = CustomAdamW(optimizer_grouped_parameters, **optimizer_kwargs)
-
-        return self.optimizer
+    # def create_optimizer(self):
+    #     opt_model = self.model
+    #     if self.optimizer is None:
+    #         decay_parameters = get_parameter_names(opt_model, ALL_LAYERNORM_LAYERS)
+    #         decay_parameters = [name for name in decay_parameters if "bias" not in name]
+    #         optimizer_grouped_parameters = [
+    #             {
+    #                 "params": [
+    #                     p for n, p in opt_model.named_parameters() if (n in decay_parameters and p.requires_grad)
+    #                 ],
+    #                 "weight_decay": self.args.weight_decay,
+    #             },
+    #             {
+    #                 "params": [
+    #                     p for n, p in opt_model.named_parameters() if (n not in decay_parameters and p.requires_grad)
+    #                 ],
+    #                 "weight_decay": 0.0,
+    #             },
+    #         ]
+    #
+    #         optimizer_cls, optimizer_kwargs = Trainer.get_optimizer_cls_and_kwargs(self.args)
+    #
+    #         self.optimizer = CustomAdamW(optimizer_grouped_parameters, **optimizer_kwargs)
+    #
+    #     return self.optimizer
 
 
 def argument_parsing(notebook=False, notebook_args=None):
@@ -490,12 +492,15 @@ if __name__ == "__main__":
     if not training_conf.deepspeed or training_conf.local_rank == 0:
         tokenizer_sanity_check(tokenizer)
     training_conf.model_name = model_name
+
+
+
     model = get_model(training_conf, tokenizer)
 
-    model = get_llama_model(model)
+    # model = get_llama_model(model)
 
-    for _, param in model.named_parameters():
-        param.requires_grad = True
+    # for _, param in model.named_parameters():
+    #     param.requires_grad = True
 
     train, evals = get_dataset(training_conf)
     train_collate_fn = DialogueDataCollator(
