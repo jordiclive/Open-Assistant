aefd059188a2aadd928c5361e9afab2312835c48
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-12 23:39:24 +0100

    no message

--- slurm_40_multi2.sh
+++ slurm_40_multi2.sh
@@ -23,6 +23,28 @@ export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_PORT=12802
 export COUNT_NODE=`scontrol show hostnames "$SLURM_JOB_NODELIST" | wc -l`
 
+export OMPI_MCA_mtl_base_verbose=1
+export LD_LIBRARY_PATH=/opt/aws-ofi-nccl/lib:/opt/amazon/efa/lib64:/usr/local/cuda-11.0/efa/lib:/usr/local/cuda-11.0/lib:/usr/local/cuda-11.0/lib64:/usr/local/cuda-11.0:/opt/nccl/build/lib:/opt/aws-ofi-nccl-install/lib:/opt/aws-ofi-nccl/lib:$LD_LIBRARY_PATH
+export PATH=/opt/amazon/efa/bin:/opt/amazon/openmpi/bin:$PATH
+export LD_PRELOAD="/opt/nccl/build/lib/libnccl.so"
+
+export NCCL_DEBUG=WARN
+export NCCL_TREE_THRESHOLD=0
+export NCCL_PROTO=simple
+# Network issues without these set; See https://github.com/NVIDIA/nccl/issues/676
+# export NCCL_P2P_DISABLE=1
+export NCCL_IBEXT_DISABLE=1
+export NCCL_SOCKET_IFNAME="eth0"
+
+
+export FI_EFA_FORK_SAFE=1
+export FI_LOG_LEVEL=1
+export FI_EFA_USE_DEVICE_RDMA=1 # use for p4dn
+export FI_EFA_ENABLE_SHM_TRANSFER=0
+export FI_PROVIDER=efa
+export FI_EFA_TX_MIN_CREDITS=64
+
+
 echo go $COUNT_NODE
 echo $HOSTNAMES
 hostfile="/admin/home-jordiclive/Open-Assistant/hostfile.txt"
@@ -43,5 +65,5 @@ cd /admin/home-jordiclive/Open-Assistant/model/model_training/
 #srun deepspeed --hostfile $DEEPSPEED_HOSTFILE /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu gpt-neox --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --num_train_epochs 8 --residual_dropout 0.2 --deepspeed --num_train_epochs 12 --gradient_accumulation_steps 1 --use_flash_attention false --residual_dropout 0.0 --learning_rate 4e-6
 source /fsx/home-jordiclive/miniconda3/bin/activate open
 
-deepspeed --num_gpus 8 --num_nodes 2 --master_addr $MASTER_ADDR --hostfile=$hostfile /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu llama-7b --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --deepspeed --residual_dropout 0.0 --learning_rate 4e-6 --use_flash_attention False
+deepspeed --num_gpus 8 --num_nodes 2 --launcher openmpi --master_addr $MASTER_ADDR --hostfile=$hostfile /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu llama-7b --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --deepspeed --residual_dropout 0.0 --learning_rate 4e-6 --use_flash_attention False
 
