5f6f95240c41147c7ad6bf8a98d39a9932e35ae0
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-12 21:28:35 +0100

    no message

--- slurm_40_multi.sh
+++ slurm_40_multi.sh
@@ -7,37 +7,39 @@
 #SBATCH --ntasks-per-node=8
 #SBATCH --output=%x_%j.out
 
-
+module load openmpi
+module load cuda/11.7
 
 source /fsx/home-jordiclive/miniconda3/bin/activate open
+
 cd /admin/home-jordiclive/Open-Assistant
 export TRANSFORMERS_CACHE=/fsx/home-jordiclive/transformers_cache
 #export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 export PYTHONPATH="/admin/home-jordiclive/Open-Assistant/model/model_training:$PYTHONPATH"
 export PYTHONPATH="/admin/home-jordiclive/Open-Assistant/model:$PYTHONPATH"
 
+export PYTHONFAULTHANDLER=1
+export CUDA_LAUNCH_BLOCKING=0
+export HOSTNAMES=`scontrol show hostnames "$SLURM_JOB_NODELIST"`
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_PORT=12802
-### get the first node name as master address - customized for vgg slurm
-### e.g. master(gnodee[2-5],gnoded1) == gnodee2
-master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_ADDR=$master_addr"i"
-echo "MASTER_ADDR="$MASTER_ADDR
+export COUNT_NODE=`scontrol show hostnames "$SLURM_JOB_NODELIST" | wc -l`
+
+echo go $COUNT_NODE
+echo $HOSTNAMES
+hostfile="hostfile.txt"
+rm -f $hostfile
+for node in $HOSTNAMES; do
+  echo $node slots=8 >> $hostfile
+done
 
 
-#export MASTER_PORT="12802"
-#master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-#export MASTER_ADDR=$master_addr
-#echo "MASTER_ADDR="$MASTER_ADDR
-echo "machine1 slots=8
-machine2 slots=8" > /tmp/loopback_hostfile
-export DEEPSPEED_HOSTFILE="/tmp/loopback_hostfile"
 #export ADDR="$(hostname -f):29500"
 #export MASTER_PORT=$(python -c 'import socket; s = socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 cd /admin/home-jordiclive/Open-Assistant/model/model_training/
 #srun deepspeed --hostfile $DEEPSPEED_HOSTFILE /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu gpt-neox --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --num_train_epochs 8 --residual_dropout 0.2 --deepspeed --num_train_epochs 12 --gradient_accumulation_steps 1 --use_flash_attention false --residual_dropout 0.0 --learning_rate 4e-6
 
-deepspeed --num_gpus 8 --num_nodes 2 --hostfile $DEEPSPEED_HOSTFILE /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu llama-7b --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --deepspeed --residual_dropout 0.0 --learning_rate 4e-6 --use_flash_attention False
-
+deepspeed --num_gpus 8 --num_nodes 2 --master_addr $MASTER_ADDR --hostfile='/fsx/home-kaizhaol/long-context-transformers/hostfile.txt' --launcher OpenMPI /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu llama-7b --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --deepspeed --residual_dropout 0.0 --learning_rate 4e-6 --use_flash_attention False
 
 
 #!/bin/bash
