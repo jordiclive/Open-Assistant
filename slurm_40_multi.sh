560182b0b9a6a7a7f0f405857b0965a16372c7dd
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-12 22:36:36 +0100

    no message

--- slurm_40_multi.sh
+++ slurm_40_multi.sh
@@ -12,13 +12,8 @@
 module load openmpi
 module load cuda/11.7
 
-source /fsx/home-jordiclive/miniconda3/bin/activate open
 
-cd /admin/home-jordiclive/Open-Assistant
-export TRANSFORMERS_CACHE=/fsx/home-jordiclive/transformers_cache
-#export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-export PYTHONPATH="/admin/home-jordiclive/Open-Assistant/model/model_training:$PYTHONPATH"
-export PYTHONPATH="/admin/home-jordiclive/Open-Assistant/model:$PYTHONPATH"
+
 
 export PYTHONFAULTHANDLER=1
 export CUDA_LAUNCH_BLOCKING=0
@@ -29,17 +24,23 @@ export COUNT_NODE=`scontrol show hostnames "$SLURM_JOB_NODELIST" | wc -l`
 
 echo go $COUNT_NODE
 echo $HOSTNAMES
-hostfile="hostfile.txt"
+hostfile="/admin/home-jordiclive/Open-Assistant/hostfile.txt"
 rm -f $hostfile
 for node in $HOSTNAMES; do
   echo $node slots=8 >> $hostfile
 done
 
 
+cd /admin/home-jordiclive/Open-Assistant
+export TRANSFORMERS_CACHE=/fsx/home-jordiclive/transformers_cache
+#export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+export PYTHONPATH="/admin/home-jordiclive/Open-Assistant/model/model_training:$PYTHONPATH"
+export PYTHONPATH="/admin/home-jordiclive/Open-Assistant/model:$PYTHONPATH"
 #export ADDR="$(hostname -f):29500"
 #export MASTER_PORT=$(python -c 'import socket; s = socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 cd /admin/home-jordiclive/Open-Assistant/model/model_training/
 #srun deepspeed --hostfile $DEEPSPEED_HOSTFILE /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu gpt-neox --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --num_train_epochs 8 --residual_dropout 0.2 --deepspeed --num_train_epochs 12 --gradient_accumulation_steps 1 --use_flash_attention false --residual_dropout 0.0 --learning_rate 4e-6
+source /fsx/home-jordiclive/miniconda3/bin/activate open
 
 deepspeed --master_addr $MASTER_ADDR --hostfile='/admin/home-jordiclive/Open-Assistant/hostfile.txt' --launcher OpenMPI /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu llama-7b --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --deepspeed --residual_dropout 0.0 --learning_rate 4e-6 --use_flash_attention False
 
