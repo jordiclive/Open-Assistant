ed99f1e7dd45a80da6add8a92ceb462790bf1f38
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-13 18:23:48 +0100

    no message

--- model/model_training/custom_datasets/prompt_dialogue.py
+++ model/model_training/custom_datasets/prompt_dialogue.py
@@ -30,7 +30,7 @@ def load_oig_file(
     max_count: Optional[int] = None,
     min_length: Optional[int] = 1000,
     manual_seed: int = 297631038922,
-) -> tuple[ListDataset, ListDataset]:
+):
     generator = Generator()
     generator.manual_seed(manual_seed)
 
