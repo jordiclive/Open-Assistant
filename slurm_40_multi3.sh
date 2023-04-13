d469b5a74a464482802dbb38ee0604a23d75828c
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-13 13:31:55 +0100

    no message

--- slurm_40_multi3.sh
+++ slurm_40_multi3.sh
@@ -44,31 +44,35 @@ export COUNT_NODE=`scontrol show hostnames "$SLURM_JOB_NODELIST" | wc -l`
 #export OMPI_MCA_btl="tcp,self"
 #export OMPI_MCA_btl_tcp_if_exclude="lo,docker1"
 #export OMPI_MCA_plm_rsh_no_tree_spawn=1
-export LD_LIBRARY_PATH=/opt/aws-ofi-nccl/lib:/opt/amazon/efa/lib64:/usr/local/cuda-11.0/efa/lib:/usr/local/cuda-11.0/lib:/usr/local/cuda-11.0/lib64:/usr/local/cuda-11.0:/opt/nccl/build/lib:/opt/aws-ofi-nccl-install/lib:/opt/aws-ofi-nccl/lib:$LD_LIBRARY_PATH
-export PATH=/opt/amazon/efa/bin:/opt/amazon/openmpi/bin:$PATH
-export LD_PRELOAD="/opt/nccl/build/lib/libnccl.so"
-
-export NCCL_DEBUG=WARN
-export NCCL_TREE_THRESHOLD=0
-export NCCL_PROTO=simple
-# Network issues without these set; See https://github.com/NVIDIA/nccl/issues/676
-# export NCCL_P2P_DISABLE=1
-export NCCL_IBEXT_DISABLE=1
-export NCCL_SOCKET_IFNAME="eth0"
-
-
-export FI_EFA_FORK_SAFE=1
-export FI_LOG_LEVEL=1
-export FI_EFA_USE_DEVICE_RDMA=1 # use for p4dn
-export FI_EFA_ENABLE_SHM_TRANSFER=0
-export FI_PROVIDER=efa
-export FI_EFA_TX_MIN_CREDITS=64
-
-export PYTHONFAULTHANDLER=1
+#export LD_LIBRARY_PATH=/opt/aws-ofi-nccl/lib:/opt/amazon/efa/lib64:/usr/local/cuda-11.0/efa/lib:/usr/local/cuda-11.0/lib:/usr/local/cuda-11.0/lib64:/usr/local/cuda-11.0:/opt/nccl/build/lib:/opt/aws-ofi-nccl-install/lib:/opt/aws-ofi-nccl/lib:$LD_LIBRARY_PATH
+#export PATH=/opt/amazon/efa/bin:/opt/amazon/openmpi/bin:$PATH
+#export LD_PRELOAD="/opt/nccl/build/lib/libnccl.so"
+#
+#export NCCL_DEBUG=WARN
+#export NCCL_TREE_THRESHOLD=0
+#export NCCL_PROTO=simple
+## Network issues without these set; See https://github.com/NVIDIA/nccl/issues/676
+## export NCCL_P2P_DISABLE=1
+#export NCCL_IBEXT_DISABLE=1
+#export NCCL_SOCKET_IFNAME="eth0"
+#
+#
+#export FI_EFA_FORK_SAFE=1
+#export FI_LOG_LEVEL=1
+#export FI_EFA_USE_DEVICE_RDMA=1 # use for p4dn
+#export FI_EFA_ENABLE_SHM_TRANSFER=0
+#export FI_PROVIDER=efa
+#export FI_EFA_TX_MIN_CREDITS=64
+#
+#export PYTHONFAULTHANDLER=1
 
 # export CUDA_LAUNCH_BLOCKING=1
 
 export OMPI_MCA_mtl_base_verbose=1
+
+export NCCL_SOCKET_IFNAME=eth0
+export NCCL_IB_HCA=ibp
+#export TORCH_EXTENSIONS_DIR=extensions
 
 
 source /fsx/home-jordiclive/miniconda3/bin/activate open
