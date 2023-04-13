7aa1c3a0edfdc4e4c629bd872f67f8673b635e72
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-13 11:21:52 +0100

    no message

--- slurm_40_multi2.sh
+++ slurm_40_multi2.sh
@@ -23,12 +23,15 @@ export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_PORT=12802
 export COUNT_NODE=`scontrol show hostnames "$SLURM_JOB_NODELIST" | wc -l`
 
+cd /admin/home-jordiclive/Open-Assistant/model/model_training/
+
+source /fsx/home-jordiclive/miniconda3/bin/activate open
 export OMPI_MCA_mtl_base_verbose=1
 export LD_LIBRARY_PATH=/opt/aws-ofi-nccl/lib:/opt/amazon/efa/lib64:/usr/local/cuda-11.0/efa/lib:/usr/local/cuda-11.0/lib:/usr/local/cuda-11.0/lib64:/usr/local/cuda-11.0:/opt/nccl/build/lib:/opt/aws-ofi-nccl-install/lib:/opt/aws-ofi-nccl/lib:$LD_LIBRARY_PATH:/usr/lib64/compat-openmpi16/lib
 export PATH=/opt/amazon/efa/bin:/opt/amazon/openmpi/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 
-export LD_PRELOAD="/opt/nccl/build/lib/libnccl.so"
+#export LD_PRELOAD="/opt/nccl/build/lib/libnccl.so"
 
 export NCCL_DEBUG=WARN
 export NCCL_TREE_THRESHOLD=0
@@ -60,9 +63,7 @@ export TRANSFORMERS_CACHE=/fsx/home-jordiclive/transformers_cache
 export PYTHONPATH="/admin/home-jordiclive/Open-Assistant/model/model_training:$PYTHONPATH"
 export PYTHONPATH="/admin/home-jordiclive/Open-Assistant/model:$PYTHONPATH"
 
-cd /admin/home-jordiclive/Open-Assistant/model/model_training/
 
-source /fsx/home-jordiclive/miniconda3/bin/activate open
 
 deepspeed --num_nodes -1 --launcher openmpi --master_addr $MASTER_ADDR --hostfile=$hostfile /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu llama-7b --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --deepspeed --residual_dropout 0.0 --learning_rate 4e-6 --use_flash_attention False
 
