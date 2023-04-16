f9c679362df55620b0596c86daac5873e4b3600e
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-16 18:34:16 +0100

    no message

--- slurm_80.sh
+++ slurm_80.sh
@@ -17,6 +17,6 @@ export PYTHONPATH="/admin/home-jordiclive/Open-Assistant/model:$PYTHONPATH"
 
 cd /admin/home-jordiclive/Open-Assistant/model/model_training/
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 deepspeed /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu my-llama-7b --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --deepspeed --residual_dropout 0.0 --learning_rate 4e-6 --use_flash_attention True
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 deepspeed /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults my-llama-7b --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --deepspeed --residual_dropout 0.0 --learning_rate 4e-6 --use_flash_attention True
 
 
