c03c6e3b9bb047b775ddfee691a52599a1fc753d
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-16 18:09:26 +0100

    no message

--- model/model_training/trainer_sft.py
+++ model/model_training/trainer_sft.py
@@ -2,7 +2,7 @@
 import argparse
 import logging
 import os
-from datetime import time
+from time import time
 from functools import partial
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
