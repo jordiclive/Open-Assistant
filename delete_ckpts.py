d12b515f3d79780392cd6b3c7907bead1baf75a4
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-17 22:31:21 +0100

    no message

--- delete_ckpts.py
+++ delete_ckpts.py
@@ -13,6 +13,20 @@ def delete_unwanted_files(directory, keep_files):
                 except Exception as e:
                     print(f"Error deleting {file_path}: {e}")
 
+def delete_everything(directory, keep_files):
+    keep_files = ['added_tokens.json']
+    for root, _, filenames in os.walk(directory):
+        for filename in fnmatch.filter(filenames, '*'):
+            if filename not in keep_files:
+                file_path = os.path.join(root, filename)
+                try:
+                    os.remove(file_path)
+                    print(f"Deleted: {file_path}")
+                except Exception as e:
+                    print(f"Error deleting {file_path}: {e}")
+
+
+
 if __name__ == "__main__":
     search_directory = "/fsx/home-jordiclive/66B_checkpoints"
     keep_files = [
@@ -30,5 +44,8 @@ if __name__ == "__main__":
     while True:
         print("Running delete_unwanted_files...")
         delete_unwanted_files(search_directory, keep_files)
+        
+        #todo remove
+        delete_everything(search_directory)
         print("Waiting for 2 hours before the next run...")
         time.sleep(60 * 60)
