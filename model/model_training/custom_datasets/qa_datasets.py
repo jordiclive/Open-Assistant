782af211c3926ae6ac19a4fc69ea6cc04d7e2da7
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-19 12:11:09 +0100

    change annotations

--- model/model_training/custom_datasets/qa_datasets.py
+++ model/model_training/custom_datasets/qa_datasets.py
@@ -590,7 +590,7 @@ class DatabricksDolly15k(Dataset):
 
 
 class AlpacaGpt4(Dataset):
-    def __init__(self, cache_dir: str | Path, mode: str = "sft", input_max_length: int = 2048) -> None:
+    def __init__(self, cache_dir: Union[str,Path], mode: str = "sft", input_max_length: int = 2048) -> None:
         super().__init__()
         self.rows = []
         if mode not in ("sft", "rl"):
@@ -601,7 +601,7 @@ class AlpacaGpt4(Dataset):
             if (conv := self._process_instruction(line, input_max_length)) is not None:
                 self.rows.append(conv)
 
-    def _process_instruction(self, row: dict[str, str], input_max_length: int) -> list[str] | None:
+    def _process_instruction(self, row: Dict[str, str], input_max_length: int) -> Union[List[str], None]:
         # discard items that are too long: when checked on 2023-04-17 this was just one item in the whole dataset with length above 2048.
         # And 12 above 1024.
         if len(row["input"]) + len(row["instruction"]) > input_max_length:
@@ -622,8 +622,8 @@ class AlpacaGpt4(Dataset):
     def __len__(self) -> int:
         return len(self.rows)
 
-    def __getitem__(self, index: int) -> list[str] | tuple[str]:
-        dialogue: list[str] = self.rows[index]
+    def __getitem__(self, index: int) -> Union[List[str],Tuple[str]]:
+        dialogue: List[str] = self.rows[index]
         if self.mode == "sft":
             return dialogue
         elif self.mode == "rl":
