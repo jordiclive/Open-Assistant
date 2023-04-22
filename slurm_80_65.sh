a47ec500af4e13417099b5ccaf99e762837a1ef8
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-22 17:40:06 +0100

    x

--- slurm_80_65.sh
+++ slurm_80_65.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition="g80"
 #SBATCH --job-name=OA
 #SBATCH --gres=gpu:8
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=12
 #SBATCH --cpus-per-task=24
 #SBATCH --output=%x_%j.out
 
