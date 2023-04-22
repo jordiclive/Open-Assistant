d7a997d6d8e1f8e29ea35dfd9c10e2a6e8766067
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-22 18:17:27 +0100

    no message

--- slurm_multi.sh
+++ slurm_multi.sh
@@ -2,7 +2,7 @@
 #SBATCH --account laion
 #SBATCH --partition="g80"
 #SBATCH --job-name=OA
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --gres=gpu:8
 #SBATCH --ntasks-per-node=8
 #SBATCH --output=%x_%j.out
