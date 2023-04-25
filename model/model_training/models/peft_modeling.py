e07e2ab6069a4d794b17bdfbcc422c4d335b3399
jordiclive <jordiclive19@imperial.ac.uk>
2023-04-25 13:47:58 +0100

    no message

--- model/model_training/models/peft_modeling.py
+++ model/model_training/models/peft_modeling.py
@@ -196,27 +196,27 @@ class PeftFlashTrainer(Trainer):
 
     def create_optimizer(self):
         opt_model = self.model
-        if self.optimizer is None:
-            decay_parameters = get_parameter_names(opt_model, ALL_LAYERNORM_LAYERS)
-            decay_parameters = [name for name in decay_parameters if "bias" not in name]
-            optimizer_grouped_parameters = [
-                {
-                    "params": [
-                        p for n, p in opt_model.named_parameters() if (n in decay_parameters and p.requires_grad)
-                    ],
-                    "weight_decay": self.args.weight_decay,
-                },
-                {
-                    "params": [
-                        p for n, p in opt_model.named_parameters() if (n not in decay_parameters and p.requires_grad)
-                    ],
-                    "weight_decay": 0.0,
-                },
-            ]
 
-            optimizer_cls, optimizer_kwargs = Trainer.get_optimizer_cls_and_kwargs(self.args)
+        decay_parameters = get_parameter_names(opt_model, ALL_LAYERNORM_LAYERS)
+        decay_parameters = [name for name in decay_parameters if "bias" not in name]
+        optimizer_grouped_parameters = [
+            {
+                "params": [
+                    p for n, p in opt_model.named_parameters() if (n in decay_parameters and p.requires_grad)
+                ],
+                "weight_decay": self.args.weight_decay,
+            },
+            {
+                "params": [
+                    p for n, p in opt_model.named_parameters() if (n not in decay_parameters and p.requires_grad)
+                ],
+                "weight_decay": 0.0,
+            },
+        ]
 
-            self.optimizer = CustomAdamW(optimizer_grouped_parameters, **optimizer_kwargs)
+        optimizer_cls, optimizer_kwargs = Trainer.get_optimizer_cls_and_kwargs(self.args)
+
+        self.optimizer = CustomAdamW(optimizer_grouped_parameters, **optimizer_kwargs)
 
         return self.optimizer
 
