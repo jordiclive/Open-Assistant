9b54e45f29be1b4f9b3743b6c8858a8890f6a431
jordiclive <jordiclive19@imperial.ac.uk>
2023-05-05 12:19:35 +0100

    no message

--- model/model_eval/manual/sampling_report.py
+++ model/model_eval/manual/sampling_report.py
@@ -282,7 +282,7 @@ def main():
     if args.model_type.lower() == "causallm" or args.model_type.lower() == "llama":
         from transformers import AutoModelForCausalLM
 
-        tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=args.auth_token)
+        # tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=args.auth_token)
         model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=args.auth_token, **model_args)
         skip_input_tokens = True
     elif args.model_type.lower() == "t5conditional":
