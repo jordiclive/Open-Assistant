2abf2b3568362a9a4f781906b280b1d824ff187a
jordiclive <jordiclive19@imperial.ac.uk>
2023-05-07 07:50:31 +0100

    no message

--- slurm_80.sh
+++ slurm_80.sh
@@ -18,7 +18,7 @@ export PYTHONPATH="/admin/home-jordiclive/peft_open_assistant/Open-Assistant/mod
 
 cd /admin/home-jordiclive/peft_open_assistant/Open-Assistant/model/model_training/
 #srun deepspeed --hostfile $DEEPSPEED_HOSTFILE /admin/home-jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst_export_eu gpt-neox --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/output_dir --num_train_epochs 8 --residual_dropout 0.2 --deepspeed --num_train_epochs 12 --gradient_accumulation_steps 1 --use_flash_attention false --residual_dropout 0.0 --learning_rate 4e-6
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 deepspeed /admin/home-jordiclive/peft_open_assistant/Open-Assistant/model/model_training/trainer_sft.py --configs defaults lora-finetune lora-llama-65b --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/peft_models_lora_65B/ --deepspeed --residual_dropout 0.0 2>&1 | tee debug.txt
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 deepspeed /admin/home-jordiclive/peft_open_assistant/Open-Assistant/model/model_training/trainer_sft.py --configs defaults lora-finetune lora-llama-65b --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/peft_models_lora_65B_4/ --deepspeed --residual_dropout 0.0 2>&1 | tee debug.txt
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 deepspeed /admin/home-jordiclive/peft_open_assistant/Open-Assistant/model/model_training/trainer_sft.py --configs defaults lora-finetune lora-llama-30b --cache_dir /fsx/home-jordiclive/data_cache --output_dir /fsx/home-jordiclive/peft_models_lora_30b_4/ --deepspeed --residual_dropout 0.0 2>&1 | tee debug.txt
 
 #CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 deepspeed /admin/home-jordiclive/peft_open_assistant/Open-Assistant/model/model
