95e2ff240646ac7eebb1f9b5131b6eb5d28b0b6d
jordiclive <jordiclive19@imperial.ac.uk>
2023-05-10 09:07:47 +0100

    no message

--- model/model_training/models/peft_modeling.py
+++ model/model_training/models/peft_modeling.py
@@ -82,7 +82,7 @@ class SaveLoraConfig:
     model_name: str = ""
     torch_ckpt_path: str = ""
     peft_type: str = "lora"
-    cache_dir: str = '.cache'
+    cache_dir: str = '/fsx/home-jordiclive/data_cache'
 
 
 def save_adapter_model_from_ckpt(save_config: SaveLoraConfig):
