7058bccc5032cad60729510607296a4f49d1f6be
jordiclive <jordiclive19@imperial.ac.uk>
2023-05-10 10:19:42 +0100

    no message

--- model/model_eval/manual/sampling_report_ds.py
+++ model/model_eval/manual/sampling_report_ds.py
@@ -301,7 +301,7 @@ def main():
         from transformers import AutoModelForCausalLM
 
         # tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=args.auth_token)
-        model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=args.auth_token, **model_args)
+        model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=args.auth_token, **model_args,cache_dir="/fsx/home-jordiclive/data_cache")
         skip_input_tokens = True
     elif args.model_type.lower() == "t5conditional":
         from transformers import T5ForConditionalGeneration
