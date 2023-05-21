fc9143c31253a202e5dfde37bd60b3bf8659b851
jordiclive <jordiclive19@imperial.ac.uk>
2023-05-21 12:10:14 +0100

    no message

--- slurm_script.sh
+++ slurm_script.sh
@@ -9,7 +9,8 @@ export PYTHONPATH="/mnt/data/jordiclive/Open-Assistant/model:$PYTHONPATH"
 #export HF_DATASETS_OFFLINE=1
 #export TRANSFORMERS_OFFLINE=1
 cd /mnt/data/jordiclive/Open-Assistant/model/model_training
-deepspeed --include=localhost:2,3,4,5,6,7 --master_port 61000 /mnt/data/jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults sft-8.1-datasets lora-llama-65b --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/65B_ckpts --deepspeed --residual_dropout 0.0 2>&1 | tee debug_30.txt
-
 deepspeed --include=localhost:2,3,4,5,6,7 --master_port 61000 /mnt/data/jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults pretrain lora-llama-65b --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/65B_ckpts --deepspeed --residual_dropout 0.0 2>&1 | tee debug_30.txt
+
+#deepspeed --include=localhost:2,3,4,5,6,7 --master_port 61000 /mnt/data/jordiclive/Open-Assistant/model/model_training/trainer_sft.py --configs defaults sft-8.1-datasets lora-llama-65b --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/65B_ckpts --deepspeed --residual_dropout 0.0 2>&1 | tee debug_30.txt
+
 
