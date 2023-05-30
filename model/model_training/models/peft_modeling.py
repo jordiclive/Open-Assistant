70261dcc7ded0592771cf427ded6ba74989a8fb9
jordiclive <jordiclive19@imperial.ac.uk>
2023-05-30 11:02:31 +0100

    no message

--- model/model_training/models/peft_modeling.py
+++ model/model_training/models/peft_modeling.py
@@ -76,7 +76,7 @@ def load_peft_ckpt(model, tokenizer,peft_ckpt_path=None):
     model.eos_token_id = tokenizer.eos_token_id
     embed_weights = torch.load("/mnt/data/jordiclive/adapter_ckpt_10500/extra_embeddings.pt",map_location=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 
-    model.base_model.model.model.embed_tokens.weight[32000:embed_weights.shape[0], :] = embed_weights.to(
+    model.base_model.model.model.embed_tokens.weight[32000:32000+embed_weights.shape[0], :] = embed_weights.to(
     model.base_model.model.model.embed_tokens.weight.dtype
     ).to(
     model.base_model.model.model.embed_tokens.weight.device
