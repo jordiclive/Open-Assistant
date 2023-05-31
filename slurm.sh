700625473ccabfe1ed011ca341f082deb61daaf0
jordiclive <jordiclive19@imperial.ac.uk>
2023-05-31 18:18:52 +0100

    no message

--- slurm.sh
+++ slurm.sh
@@ -8,7 +8,7 @@ export PYTHONPATH="/mnt/data/jordiclive/falcon/Open-Assistant/model:$PYTHONPATH"
 #export TRANSFORMERS_OFFLINE=1
 cd /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training
 #CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 deepspeed /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst-top1 falcon-40b --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/falcon 2>&1 --deepspeed | tee debug_30.txt
-CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0,1
 #deepspeed --include=localhost:0,1,2,3,4,5,6,7 --master_port 61000 /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst-top1 falcon-40b --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/falcon 2>&1 --deepspeed | tee debug_30.txt
 deepspeed --include=localhost:0,1 --master_port 61500 /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst-top1 falcon-7b --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/falcon 2>&1 --deepspeed | tee debug_falcon.txt
 
