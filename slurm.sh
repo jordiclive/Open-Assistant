4f5d5189d544808d353b6902ce0c6dd7c88ffa6f
jordiclive <jordiclive19@imperial.ac.uk>
2023-05-31 19:45:11 +0100

    no message

--- slurm.sh
+++ slurm.sh
@@ -13,7 +13,7 @@ cd /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training
 #CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 deepspeed /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst-top1 falcon-40b --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/falcon 2>&1 --deepspeed | tee debug_30.txt
 #export CUDA_VISIBLE_DEVICES=0,1
 #deepspeed --include=localhost:0,1,2,3,4,5,6,7 --master_port 61000 /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst-top1 falcon-40b --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/falcon 2>&1 --deepspeed | tee debug_30.txt
-deepspeed --include=localhost:0,1 --master_port 61500 /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst-top1 falcon-7b-lora --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/falcon 2>&1 --deepspeed | tee debug_falcon.txt
+deepspeed --include=localhost:0,1 --master_port 61500 /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst-top1 falcon-40b-lora --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/falcon 2>&1 --deepspeed | tee debug_falcon.txt
 
 
 #CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES="0,1" /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training/trainer_sft.py --configs defaults sft-8-datasets falcon-7b --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/falcon --residual_dropout 0.0 2>&1 | tee debug_30.txt
