d053f8de76ad42560c36491a521a77284162216c
jordiclive <jordiclive19@imperial.ac.uk>
2023-05-31 18:49:59 +0100

    no message

--- slurm.sh
+++ slurm.sh
@@ -3,8 +3,10 @@ source /mnt/data/jordiclive/jordan/bin/activate
 
 cd /mnt/data/jordiclive/falcon/Open-Assistant
 export TRANSFORMERS_CACHE=/mnt/data/jordiclive/transformers_cache
-export PYTHONPATH="/mnt/data/jordiclive/falcon/Open-Assistant/model/model_training:$PYTHONPATH"
-export PYTHONPATH="/mnt/data/jordiclive/falcon/Open-Assistant/model:$PYTHONPATH"
+#export PYTHONPATH="/mnt/data/jordiclive/falcon/Open-Assistant/model/model_training:$PYTHONPATH"
+#export PYTHONPATH="/mnt/data/jordiclive/falcon/Open-Assistant/model:$PYTHONPATH"
+export PYTHONPATH="/mnt/data/jordiclive/Open-Assistant/model/model_training:$PYTHONPATH"
+export PYTHONPATH="/mnt/data/jordiclive/Open-Assistant/model:$PYTHONPATH"
 
 #export HF_DATASETS_OFFLINE=1
 #export TRANSFORMERS_OFFLINE=1
