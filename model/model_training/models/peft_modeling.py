b5ba260d91ce1618f02b5b6729298b2d709e64d0
jordiclive <jordiclive19@imperial.ac.uk>
2023-06-01 09:27:09 +0100

    no message

--- model/model_training/models/peft_modeling.py
+++ model/model_training/models/peft_modeling.py
@@ -5,8 +5,20 @@ import torch
 from huggingface_hub import hf_hub_download
 from model_training.utils.utils import get_model, get_tokenizer
 from peft import LoraConfig, PeftModel, PrefixTuningConfig, get_peft_model, prepare_model_for_int8_training
-from peft.tuners.lora import LoraLayer
 
+def transfer_embeddings(model,embed_path,tokenizer):
+    old_embeddings = model.get_input_embeddings()
+    old_num_tokens, old_embedding_dim = old_embeddings.weight.size()
+    new_embeddings = torch.nn.Embedding(old_num_tokens, old_embedding_dim)
+    new_embeddings.to(old_embeddings.weight.device, dtype=old_embeddings.weight.dtype)
+    model._init_weights(new_embeddings)
+    embed_weights = torch.load(embed_path,map_location=old_embeddings.weight.device)
+    vocab_size = tokenizer.vocab_size
+    new_embeddings.weight.data[:vocab_size, :] = old_embeddings.weight.data[:vocab_size, :]
+    new_embeddings.weight.data[vocab_size:vocab_size + embed_weights.shape[0], :] = embed_weights.weight.data.to(
+        new_embeddings.weight.dtype).to(new_embeddings.weight.device)
+    model.set_input_embeddings(new_embeddings)
+    model.tie_weights()
 
 def load_peft_model(model, peft_model_path, tokenizer):
     model.resize_token_embeddings(len(tokenizer))
@@ -20,10 +32,13 @@ def load_peft_model(model, peft_model_path, tokenizer):
     )
     model.eos_token_id = tokenizer.eos_token_id
     extra_embeds = hf_hub_download(peft_model_path, "extra_embeddings.pt")
-    embed_weights = torch.load(extra_embeds, map_location=model.device)
-    model.base_model.model.model.embed_tokens.weight[len(tokenizer) - embed_weights.shape[0] :, :] = embed_weights.to(
-        model.base_model.model.model.embed_tokens.weight.dtype
-    )
+    transfer_embeddings(model,peft_model_path.joinpath('extra_embeddings.pt'),tokenizer)
+    return model
+
+def load_peft_finetuned_model(model,peft_model_path,tokenizer):
+    transfer_embeddings(model,peft_model_path.joinpath('extra_embeddings.pt'),tokenizer)
+    adapters_weights = torch.load(Path(peft_model_path).joinpath('adapter_model.bin'),map_location=model.device)
+    model.load_state_dict(adapters_weights, strict=False)
     return model
 
 
@@ -43,40 +58,23 @@ def prepare_model_for_gradient_checkpointing(model):
     return model
 
 
-# def find_all_linear_names(args, model):
-#     cls = bnb.nn.Linear4bit if args.bits == 4 else (bnb.nn.Linear8bitLt if args.bits == 8 else torch.nn.Linear)
-#     lora_module_names = set()
-#     for name, module in model.named_modules():
-#         if isinstance(module, cls):
-#             names = name.split('.')
-#             lora_module_names.add(names[0] if len(names) == 1 else names[-1])
-#
-#
-#     if 'lm_head' in lora_module_names: # needed for 16-bit
-#         lora_module_names.remove('lm_head')
-#     return list(lora_module_names)
-
-# def get_peft_model(model,peft_type='lora',int8_training=False, gradient_checkpointing=False):
-#     falcon= ['dense_4h_to_h', 'dense', 'query_key_value', 'dense_h_to_4h']
-#     llama = ['down_proj', 'k_proj', 'q_proj', 'gate_proj', 'o_proj', 'up_proj', 'v_proj']
-#     # modules = find_all_linear_names(args, model)
-#     modules = falcon
-#     config = LoraConfig(
-#         r=args.lora_r,
-#         lora_alpha=args.lora_alpha,
-#         target_modules=modules,
-#         lora_dropout=args.lora_dropout,
-#         bias="none",
-#         task_type="CAUSAL_LM",
-#     )
-
-
-def peft_model(model, peft_type="lora", int8_training=False, gradient_checkpointing=False, bf16=True):
+def peft_model(model, model_name, peft_type="lora", int8_training=False, gradient_checkpointing=False):
     if peft_type == "lora":
+        if "falcon" in model_name:
+            target_modules = ["dense_4h_to_h", "dense", "query_key_value", "dense_h_to_4h"]
+            r = 64
+        elif "llama" in model_name:
+            target_modules = ['down_proj', 'k_proj', 'q_proj', 'gate_proj', 'o_proj', 'up_proj', 'v_proj']
+            if '65' in model_name:
+                r = 16
+            else:
+                r = 64
+        else:
+            raise ValueError(f"Invalid model name '{model_name}'. The model name should contain 'falcon' or 'llama', Simply find target_modules for it")
         config = LoraConfig(
-            r=64,
+            r=r,
             lora_alpha=16,
-            target_modules=["dense_4h_to_h", "dense", "query_key_value", "dense_h_to_4h"],
+            target_modules=target_modules,
             lora_dropout=0.05,
             bias="none",
             task_type="CAUSAL_LM",
@@ -93,18 +91,6 @@ def peft_model(model, peft_type="lora", int8_training=False, gradient_checkpoint
 
     if gradient_checkpointing:
         model = prepare_model_for_gradient_checkpointing(model)
-
-    # for name, module in model.named_modules():
-    #     if isinstance(module, LoraLayer):
-    #         if bf16:
-    #             module = module.to(torch.bfloat16)
-    #     if 'norm' in name:
-    #         module = module.to(torch.float32)
-    #     if 'lm_head' in name or 'embed_tokens' in name:
-    #         if hasattr(module, 'weight'):
-    #             if args.bf16 and module.weight.dtype == torch.float32:
-    #                 module = module.to(torch.bfloat16)
-
     model.print_trainable_parameters()
     return model
 
@@ -117,13 +103,15 @@ class SaveLoraConfig:
     seq2seqmodel: bool = False
     freeze_layer: bool = False
     residual_dropout: float = 0
+    residual_dropout_lima: float = 0.3
     use_flash_attention: bool = False
     adapter_save_path: str = "adapter"
     cache_dir: str = ""
     model_name: str = ""
     torch_ckpt_path: str = ""
     peft_type: str = "lora"
-    residual_dropout_lima: float = 0.3
+
+
 
 def save_adapter_model_from_ckpt(save_config: SaveLoraConfig):
     tokenizer = get_tokenizer(save_config)
@@ -131,11 +119,10 @@ def save_adapter_model_from_ckpt(save_config: SaveLoraConfig):
     model = peft_model(model)
     model.load_state_dict(torch.load(save_config.torch_ckpt_path))
     vocab_size = tokenizer.vocab_size
-    num_special_tokens = len(tokenizer.additional_special_tokens)
-
-    new_embs = model.state_dict()["base_model.model.model.embed_tokens.weight"][
-        vocab_size : vocab_size + num_special_tokens, :
-    ].clone()
+    print(f"Vocab size is {vocab_size}, and new tokenizer length is {len(tokenizer)}")
+    old_embeddings = model.get_input_embeddings()
+    #todo saving more embedding than necessary, (pad embeds as well)
+    new_embs = old_embeddings.weight.data[vocab_size :, :].clone()
     new_embs = new_embs.to(save_config.dtype)
     model.save_pretrained(save_config.adapter_save_path, torch_dtype=save_config.dtype)
     tokenizer.save_pretrained(save_config.adapter_save_path)
