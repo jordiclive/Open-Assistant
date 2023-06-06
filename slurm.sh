f6dbbb44488c45e79780b5f20b8bca84ea540086
jordiclive <jordiclive19@imperial.ac.uk>
2023-06-06 21:41:03 +0100

    no message

--- slurm.sh
+++ slurm.sh
@@ -10,4 +10,4 @@ export HF_DATASETS_CACHE="/p/project/ccstdl/clive1/transformers_cache"
 export HF_HOME="/p/project/ccstdl/clive1/transformers_cache"
 
 
-deepspeed --include=localhost:0,1,2,3,4,5,6,7 --master_port 61500 /p/project/ccstdl/clive1/Open-Assistant/model/model_training/trainer_sft.py --configs defaults dolly15k pythia-70m-deduped --cache_dir /p/project/ccstdl/clive1/data_cache --output_dir /p/project/ccstdl/clive1/output_dir --deepspeed 2>&1 | tee debug_falcon.txt
+deepspeed --include=localhost:0,1,2,3 --master_port 61500 /p/project/ccstdl/clive1/Open-Assistant/model/model_training/trainer_sft.py --configs defaults dolly15k pythia-70m-deduped --cache_dir /p/project/ccstdl/clive1/data_cache --output_dir /p/project/ccstdl/clive1/output_dir --deepspeed 2>&1 | tee debug_falcon.txt
