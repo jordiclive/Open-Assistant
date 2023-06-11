63e4d11eaab37d750413821185213599c6d3cf91
jordiclive <jordiclive19@imperial.ac.uk>
2023-06-11 18:54:21 +0100

    no message

---
+++ model_save.sh
@@ -0,0 +1,16 @@
+source /mnt/data/jordiclive/falcon_lora/bin/activate
+
+cd /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training/models
+
+export PYTHONPATH="/mnt/data/jordiclive/falcon_lora/Open-Assistant/model/model_training:$PYTHONPATH"
+export PYTHONPATH="/mnt/data/jordiclive/falcon_lora/Open-Assistant/model:$PYTHONPATH"
+export HOME="/mnt/data/jordiclive"
+export TMP="/mnt/data/jordiclive"
+export TEMP="/mnt/data/jordiclive"
+export TMPDIR="/mnt/data/jordiclive"
+export TRANSFORMERS_CACHE="/mnt/data/jordiclive/transformers_cache"
+export HF_DATASETS_CACHE="/mnt/data/jordiclive/transformers_cache"
+export HF_HOME="/mnt/data/jordiclive/transformers_cache"
+
+
+python peft_modeling.py
