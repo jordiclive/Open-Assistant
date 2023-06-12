e1b99304ee428f5868a896cd89aa4704c9dea27f
jordiclive <jordiclive19@imperial.ac.uk>
2023-06-12 13:30:16 +0100

    no message

---
+++ delete_ds.sh
@@ -0,0 +1,13 @@
+dir="/mnt/data/jordiclive/falcon/sft_falcon_ckpts"
+
+while true; do
+    # Find directories and sort by step number, keeping the two highest
+    dirs_to_delete=$(find "$dir" -type d -regex ".*/global_step[0-9]*" | sort -t '_' -k2 -n | head -n -2)
+    for folder in $dirs_to_delete; do
+        echo "Deleting: $folder"
+        rm -rf "$folder"
+    done
+
+    # Sleep for 45 minutes
+    sleep 2700
+done
