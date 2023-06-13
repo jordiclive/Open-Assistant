47b5a6065f0b3bcecfba7a332632f98d261fb541
jordiclive <jordiclive19@imperial.ac.uk>
2023-06-13 09:59:10 +0100

    no message

--- model_save.sh
+++ model_save.sh
@@ -2,15 +2,15 @@ source /mnt/data/jordiclive/jordan_falcon/bin/activate
 
 cd /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training/models
 
-export PYTHONPATH="/mnt/data/jordiclive/falcon/Open-Assistant/model/model_training:$PYTHONPATH"
-export PYTHONPATH="/mnt/data/jordiclive/falcon/Open-Assistant/model:$PYTHONPATH"
+export PYTHONPATH="/mnt/data/jordiclive/falcon_lora/Open-Assistant/model/model_training:$PYTHONPATH"
+export PYTHONPATH="/mnt/data/jordiclive/falcon_lora/Open-Assistant/model:$PYTHONPATH"
 export HOME="/mnt/data/jordiclive"
-#export TMP="/mnt/data/jordiclive"
-#export TEMP="/mnt/data/jordiclive"
-#export TMPDIR="/mnt/data/jordiclive"
+export TMP="/mnt/data/jordiclive"
+export TEMP="/mnt/data/jordiclive"
+export TMPDIR="/mnt/data/jordiclive"
 export TRANSFORMERS_CACHE="/mnt/data/jordiclive/transformers_cache"
 export HF_DATASETS_CACHE="/mnt/data/jordiclive/transformers_cache"
 export HF_HOME="/mnt/data/jordiclive/transformers_cache"
 
 
-python peft_modeling.py 2>&1 | tee debug_falcon.txt
+python peft_modeling.py
