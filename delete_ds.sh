fe4ca8075b080c8732932f457011aa159d0f11ae
jordiclive <jordiclive19@imperial.ac.uk>
2023-06-13 13:59:32 +0100

    no message

--- delete_ds.sh
+++ delete_ds.sh
@@ -2,7 +2,7 @@ dir="/mnt/data/jordiclive/falcon/sft_falcon_ckpts"
 
 while true; do
     # Find directories and sort by step number, keeping the two highest
-    dirs_to_delete=$(find "$dir" -mindepth 1 -maxdepth 1 -type d -regex ".*/checkpoint-[0-9]*" | sort -t '-' -k2 -n | head -n -2)
+    dirs_to_delete=$(find "$dir" -type d -regex ".*/global_step[0-9]*" | sort -t '_' -k2 -n | head -n -2)
     for folder in $dirs_to_delete; do
         echo "Deleting: $folder"
         rm -rf "$folder"
