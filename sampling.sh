1dca7429048b34e664da92f41a9ae747b3e8e2a9
jordiclive <jordiclive19@imperial.ac.uk>
2023-07-12 15:54:44 +0100

    no message

--- sampling.sh
+++ sampling.sh
@@ -1,4 +1,4 @@
-source /mnt/data/jordiclive/falcon_lora/bin/activate
+conda activate /mnt/data/jordiclive/jordan_scaled
 
 cd /mnt/data/jordiclive/falcon/Open-Assistant/model/model_eval/manual
 
@@ -13,4 +13,4 @@ export HF_DATASETS_CACHE="/mnt/data/jordiclive/transformers_cache"
 export HF_HOME="/mnt/data/jordiclive/transformers_cache"
 
 
-deepspeed --include=localhost:0,1,2,3,4,5,6,7 --master_port 61500 sampling_report_ds2.py --model-name jordiclive/falcon_lora_40b_ckpt_500_oasst_1_merged --config config/noprefix2.json --prompts data/prompt_lottery_en_250_text.jsonl --verbose --mode v2_5  2>&1 | tee debug_falcon.txt
+deepspeed --include=localhost:2,3,4,5,6,7 --master_port 61500 sampling_report_ds2.py --model-name jordiclive/falcon_lora_40b_ckpt_500_oasst_1_merged --config config/noprefix2.json --prompts data/prompt_lottery_en_250_text.jsonl --verbose --mode v2_5  2>&1 | tee debug_falcon.txt
