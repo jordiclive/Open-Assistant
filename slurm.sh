c09cddd558eb7c439164fb0c31da9ecf52d1adea
jordiclive <jordiclive19@imperial.ac.uk>
2023-07-12 15:53:33 +0100

    no message

--- slurm.sh
+++ slurm.sh
@@ -13,4 +13,4 @@ export HF_DATASETS_CACHE="/mnt/data/jordiclive/transformers_cache"
 export HF_HOME="/mnt/data/jordiclive/transformers_cache"
 
 
-deepspeed --include=localhost:0,1,2,3,4,5,6,7 --master_port 61500 /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst-top1 falcon-40b-lora2 --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/falcon/sft_falcon_ckpts --deepspeed 2>&1 | tee debug_falcon.txt
+deepspeed --include=localhost:2,3,4,5,6,7 --master_port 61500 /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training/trainer_sft.py --configs defaults oasst-top1 falcon-40b-lora2 --cache_dir /mnt/data/jordiclive/data_cache --output_dir /mnt/data/jordiclive/falcon/sft_falcon_ckpts --deepspeed 2>&1 | tee debug_falcon.txt
