1885226d80fe77fb380384a8681feea77f0e221b
jordiclive <jordiclive19@imperial.ac.uk>
2023-07-24 10:04:34 +0100

    no message

--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@ conda activate /mnt/data/jordiclive/jordan_lora
 
 cd /mnt/data/jordiclive/falcon/Open-Assistant/model/model_training
 
-export PYTHONPATH="/mnt/data/jordiclive/falcon_lora/Open-Assistant/model/model_training:$PYTHONPATH"
-export PYTHONPATH="/mnt/data/jordiclive/falcon_lora/Open-Assistant/model:$PYTHONPATH"
+export PYTHONPATH="/mnt/data/jordiclive/falcon/Open-Assistant/model/model_training:$PYTHONPATH"
+export PYTHONPATH="/mnt/data/jordiclive/falcon/Open-Assistant/model:$PYTHONPATH"
 export HOME="/mnt/data/jordiclive"
 export TMP="/mnt/data/jordiclive"
 export TEMP="/mnt/data/jordiclive"
