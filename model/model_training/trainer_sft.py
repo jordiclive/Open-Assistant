59d3820db07c989c500b2b35d1138b6ac416e1fb
jordiclive <jordiclive19@imperial.ac.uk>
2023-07-24 12:58:42 +0100

    no message

--- model/model_training/trainer_sft.py
+++ model/model_training/trainer_sft.py
@@ -428,7 +428,7 @@ def main():
     # tokenizer.save_pretrained("/mnt/data/llama2/Llama-2-7b-hf-sp")
     # import time
     # time.sleep(60*30)
-    raise ValueError("Done")
+    # raise ValueError("Done")
     superhot = RopePatch.from_config(training_conf) if training_conf.superhot else None
     if superhot:
         superhot.patch(model)
